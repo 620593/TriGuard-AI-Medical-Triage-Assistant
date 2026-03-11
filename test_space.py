1c43d3abe9d64994150d623cc9976ad3a869fd78
620593 <scs620593@gmail.com>
2026-03-12 00:07:39 +0530

    updated docker

--- test_space.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
-
-app = FastAPI()
-
-@app.get("/test")
-def test():
-    return RedirectResponse("http://localhost:5173/dashboard?token=123&name=John Doe&uid=456")
